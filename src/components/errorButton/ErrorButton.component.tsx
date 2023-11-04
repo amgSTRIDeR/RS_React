ede737cbb3947c8b8c523bd68e90740bec816715
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-11-04 16:06:59 +0400

    refactor: error button component to functional

--- src/components/errorButton/ErrorButton.component.tsx
+++ src/components/errorButton/ErrorButton.component.tsx
@@ -1,27 +1,20 @@
-import React from 'react';
 import { ErrorButtonProps } from '../../shared/interfaces';
 import './ErrorButton.css';
 
-class ErrorButton extends React.Component<ErrorButtonProps> {
-  constructor(props: ErrorButtonProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="errorButton__container">
-        <img
-          onClick={this.props.showTestError}
-          className="errorButton__image"
-          src="/20-minutes-adventure-before.gif"
-          alt=""
-        />
-        <p className="errorButton__text">
-          &#x2191; click to call an error &#x2191;
-        </p>
-      </div>
-    );
-  }
-}
+const ErrorButton = (props: ErrorButtonProps) => {
+  return (
+    <div className="errorButton__container">
+      <img
+        onClick={props.showTestError}
+        className="errorButton__image"
+        src="/20-minutes-adventure-before.gif"
+        alt=""
+      />
+      <p className="errorButton__text">
+        &#x2191; click to call an error &#x2191;
+      </p>
+    </div>
+  );
+};
 
 export default ErrorButton;
