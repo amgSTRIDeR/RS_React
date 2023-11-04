e7830c473670ed857259dad33462bd3d17d5b73c
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-11-04 15:59:04 +0400

    refactor: character component to functional

--- src/components/character/Character.component.tsx
+++ src/components/character/Character.component.tsx
@@ -1,41 +1,35 @@
-import React from 'react';
 import { CharacterProps } from '../../shared/interfaces';
 import './Character.css';
 import Image from '../image/Image.component';
 
-class CharacterComponent extends React.Component<CharacterProps> {
-  constructor(props: CharacterProps) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="character">
-        <Image
-          className="character__image"
-          src={`https://rickandmortyapi.com/api/character/avatar/${this.props.id}.jpeg`}
-          alt={this.props.name}
-        />
-        <h2 className="character__name">{this.props.name}</h2>
-        <p>
-          <span className="character__text">Species</span>:&nbsp;
-          {this.props.species}
-        </p>
+const CharacterComponent = (props: CharacterProps) => {
+  return (
+    <div className="character">
+      <Image
+        className="character__image"
+        src={`https://rickandmortyapi.com/api/character/avatar/${props.id}.jpeg`}
+        alt={props.name}
+      />
+      <h2 className="character__name">{props.name}</h2>
+      <p>
+        <span className="character__text">Species</span>:&nbsp;
+        {props.species}
+      </p>
 
-        <p>
-          <span className="character__text">Origin</span>:&nbsp;
-          {this.props.origin}
-        </p>
-        <p>
-          <span className="character__text">Location</span>:&nbsp;
-          {this.props.location}
-        </p>
-        <p>
-          <span className="character__text">Status</span>:&nbsp;
-          {this.props.status}
-        </p>
-      </div>
-    );
-  }
-}
+      <p>
+        <span className="character__text">Origin</span>:&nbsp;
+        {props.origin}
+      </p>
+      <p>
+        <span className="character__text">Location</span>:&nbsp;
+        {props.location}
+      </p>
+      <p>
+        <span className="character__text">Status</span>:&nbsp;
+        {props.status}
+      </p>
+    </div>
+  );
+};
 
 export default CharacterComponent;
