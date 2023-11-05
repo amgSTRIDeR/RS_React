f03a78ac8de0e81d36cbd0ba31db9e9ab71ddd83
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-11-06 03:32:05 +0400

    feat: aside section

--- src/components/character/Character.component.tsx
+++ src/components/character/Character.component.tsx
@@ -1,10 +1,23 @@
 import { CharacterProps } from '../../shared/interfaces';
 import './Character.css';
 import Image from '../image/Image.component';
+import { useSearchParams } from 'react-router-dom';
 
 const CharacterComponent = (props: CharacterProps) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const changeQueryParams = () => {
+    const isAsideShown =
+      searchParams.has('aside') && searchParams.get('aside') === '1';
+
+    setSearchParams((prevSearchParams) => {
+      prevSearchParams.set('aside', `${isAsideShown ? '0' : '1'}`);
+      prevSearchParams.set('details', `${props.id}`);
+      return prevSearchParams;
+    });
+  };
   return (
-    <div className="character">
+    <div className="character" onClick={changeQueryParams}>
       <Image
         className="character__image"
         src={`https://rickandmortyapi.com/api/character/avatar/${props.id}.jpeg`}
